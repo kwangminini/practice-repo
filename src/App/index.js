51f612497d4259e30258ef776bfe529c3b9a3ffe
kwangminini <rhkdals7362@gmail.com>
2023-01-18 01:19:41 +0900

    update: add emotion styles test

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import styles from "./styles.scss";
+import styled from "@emotion/styled";
+
+const Test = styled.div`
+  color: red;
+`;
 
 function App() {
-  return <div className={styles.test}>Hello</div>;
+  return (
+    <div className={styles.test}>
+      Hello
+      <Test>fontColor:red</Test>
+    </div>
+  );
 }
 
 export default App;
