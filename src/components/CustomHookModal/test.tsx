83954b06adb66870d4ee635f929eaf1fbc17c860
kwangminini <rhkdals7362@gmail.com>
2023-02-18 02:04:21 +0900

    add modal description text and test code

--- src/components/CustomHookModal/test.tsx
+++ src/components/CustomHookModal/test.tsx
@@ -15,6 +15,27 @@ describe("CustomHookModal", () => {
 
   it("render modal button", () => {
     renderComponent();
-    screen.getAllByText(/modal/);
+
+    const modalButton = screen.getAllByText("open modal");
+    expect(modalButton).toHaveLength(3);
+  });
+
+  it("render modal description text", () => {
+    renderComponent();
+
+    const parentStateModalText = screen.getByText(
+      "부모컴포넌트 state를 이용한 모달 띄우는 방식"
+    );
+    expect(parentStateModalText).toBeVisible();
+
+    const customHookModalText = screen.getByText(
+      "custom hook을 이용한 모달 띄우는 방식"
+    );
+    expect(customHookModalText).toBeVisible();
+
+    const notImportCustomHookModalText = screen.getByText(
+      "import modal 없이custom hook을 이용한 모달 띄우는 방식"
+    );
+    expect(notImportCustomHookModalText).toBeVisible();
   });
 });
