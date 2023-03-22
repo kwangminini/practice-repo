deef3c74000a4dfaf717118d7e1ed688ca9eeee8
kwangminini <rhkdals7362@gmail.com>
2023-03-22 22:37:05 +0900

    create axios instance

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const COIN_BASE_URL = "https://api.coinranking.com/v2/";
+
+const axiosInstance = (url: string, options?: any) => {
+  const instance = axios.create({
+    baseURL: url,
+    ...options,
+  });
+  return instance;
+};
+
+export const coinAPI = axiosInstance(COIN_BASE_URL);
