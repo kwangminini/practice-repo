a79c59a1f0bbdd28a0ef84468b66fffbba43120f
kwangminini <rhkdals7362@gmail.com>
2023-03-24 22:40:50 +0900

    add comment

--- src/api/coin/index.ts
+++ src/api/coin/index.ts
@@ -1,5 +1,9 @@
 import { coinAPI } from "../axiosInstance";
 
+/**
+ * 코인 리스트 조회
+ * @returns 코인 리스트
+ */
 export const getCoinList = async () => {
   const response = await coinAPI.get("coins");
   return response.data;
