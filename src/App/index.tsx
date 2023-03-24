5ee06579f50cd7f5c314d5da494f1b9485a315ef
kwangminini <rhkdals7362@gmail.com>
2023-03-25 00:10:13 +0900

    feat: api-structure route

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CustomHookModal from "../pages/CustomHookModal";
 import Emotion from "../components/Emotion";
+import ApiStructure from "@/pages/ApiStructure";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Emotion />} />
       <Route path="/customhookmodal" element={<CustomHookModal />} />
+      <Route path="/api-structure" element={<ApiStructure />} />
     </Routes>
   );
 }
