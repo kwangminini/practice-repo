733d5d1487539565e16fd19d8149a74c5290790a
kwangminini <rhkdals7362@gmail.com>
2023-03-25 08:31:55 +0900

    feat: fetch coinList and render coinList

--- src/pages/ApiStructure/index.tsx
+++ src/pages/ApiStructure/index.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "@emotion/styled";
+import { getCoinList } from "@/api/coin";
 
 export default function ApiStructure() {
-  return <h1>Api Structure Page</h1>;
+  const [coinList, setCoinList] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+      const coinList = await getCoinList();
+      setCoinList(coinList.slice(1, 10));
+    })();
+  }, []);
+
+  return (
+    <>
+      <h1>Api Structure Page</h1>
+      {coinList && coinList.map((coin) => <p key={coin.id}>{coin.name}</p>)}
+    </>
+  );
 }
