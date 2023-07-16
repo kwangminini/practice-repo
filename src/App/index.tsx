7ce1e22c3c289c7e93a5eb9a13992bc64a1a99c6
kwangminini <rhkdals7362@gmail.com>
2023-07-16 22:33:49 +0900

    feat: init pdf test

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -4,6 +4,7 @@ import CustomHookModal from "../pages/CustomHookModal";
 import Emotion from "../components/Emotion";
 import ApiStructure from "@/pages/ApiStructure";
 import ApiStructureByClass from "@/pages/ApiStructureByClass";
+import PdfTest from "@/components/PdfTest";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/customhookmodal" element={<CustomHookModal />} />
       <Route path="/api-structure" element={<ApiStructure />} />
       <Route path="/api-structure-class" element={<ApiStructureByClass />} />
+      <Route path="/pdf-test" element={<PdfTest />} />
     </Routes>
   );
 }
